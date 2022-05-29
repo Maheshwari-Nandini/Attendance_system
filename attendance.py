cd06784786594d2b9fe109de324291df5fbcddd5
Nandini <maheshwarinandini07@gmail.com>
2022-05-29 17:49:40 +0530

    second

--- attendance.py
+++ attendance.py
@@ -45,7 +45,7 @@ cap=cv2.VideoCapture(0)
 
 while True:
     success,img= cap.read()
-    imgS=cv2.resize(img,None,0.25,0.25)
+    imgS=cv2.resize(img,(0,0),None,0.25,0.25)
     img=cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
 
     faceCurFrames=face_recognition.face_locations(imgS)
@@ -60,12 +60,12 @@ while True:
              name=classNames[matchIndex].upper()
              y1,x2,y2,x1=faceloc
              y1,x2,y2,x1= y1*4,x2*4,y2*4,x1*4
-             cv2.rectangle(img(x1,y1),(x2,y2),(90,255,0),2)
+             cv2.rectangle(img,(x1,y1),(x2,y2),(90,255,0),2)
              cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
              cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
              markAttendance(name)
-    cv2.imshow('webcam',img)
-    cv2.waitKey(0)
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
     
 
 
